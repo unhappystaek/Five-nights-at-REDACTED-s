8c902ee3405e0f886d0c3a2c858fa8bc141b99e0
unhappystaek <oli.ryn@wp.pl>
2023-02-05 20:25:54 +0100

    Working right door

--- scripts/Door_right.gd
+++ scripts/Door_right.gd
@@ -7,16 +7,14 @@ func _ready():
 	pass 
 
 
-
-
-
-
-func _on_button_door_left_input_event(camera, event, click_position, click_normal, shape_idx):
+func _on_button_door_right_input_event(camera, event, click_position, click_normal, shape_idx):
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
 		if doorOpen == true :
 			doorOpen = false
-			$Door_close_left.play("Door_close_left")
+			$Door_close_right.play("Door_close_right")
 		else:
 			doorOpen = true
-			$Door_open_left.play("Door_open_left")
+			$Door_open_right.play("Door_open_right")
+
+
