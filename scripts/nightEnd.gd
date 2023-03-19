d1dbd5815e79c07993607b8183e0c86745bea7bc
unhappystaek <oli.ryn@wp.pl>
2023-03-19 23:22:02 +0100

    started night win screen

---
+++ scripts/nightEnd.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var _timer = null
+var _timer2 = null
+var animation: bool = false
+
+func _ready():
+	$AudioStreamPlayer.play()
+	_timer = Timer.new()
+	add_child(_timer)
+
+	_timer.connect("timeout", self, "_on_timer_timeout")
+	_timer.set_wait_time(1.5)
+	_timer.set_one_shot(true) # Make sure it loops
+	_timer.start()
+	
+func _process(delta):
+	if animation == true and $hour.rect_position.y > 110:
+		$hour.rect_position.y -= 1
+
+func _on_timer_timeout():
+	animation = true
+	_timer2 = Timer.new()
+	add_child(_timer2)
+
+	_timer2.connect("timeout", self, "_on_timer2_timeout")
+	_timer2.set_wait_time(2.4)
+	_timer2.set_one_shot(true) # Make sure it loops
+	_timer2.start()
+
+	
+func _on_timer2_timeout():
+	if SaveSystem.night == 5:
+		pass
+	else:
+		if SaveSystem.night != 7:
+			SaveSystem.night += 1
+			get_tree().change_scene("res://scenes/actuall_scenes/Menu.tscn")
+		else:
+			get_tree().change_scene("res://scenes/actuall_scenes/Menu.tscn")
