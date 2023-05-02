f63579f95df2ae20f3df9ecafa1ab6d84016de03
unhappystaek <oli.ryn@wp.pl>
2023-05-02 02:20:37 +0200

    phone guy is now working for nights 1-4

--- scripts/Front_area.gd
+++ scripts/Front_area.gd
@@ -1,10 +1,18 @@
 extends Spatial
 
-
+var _timer = null
 var is_energy: bool = true
+var phone_waiting: bool = false
 
+func _ready():
+	_timer = Timer.new()
+	add_child(_timer)
 
-# Called when the node enters the scene tree for the first time.
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.set_wait_time(5.0)
+	_timer.set_one_shot(true) # Make sure it loops
+	_timer.start()
+
 func _process(delta):
 	if is_energy == true:
 		if SaveSystem.night == 1:
@@ -37,5 +45,19 @@ func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx
 
 func _on_phoneArea_input_event(camera, event, position, normal, shape_idx):
 	var mouse_click = event as InputEventMouseButton
-	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
-		pass
+	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed and phone_waiting == true:
+		phone_waiting = false
+		$phone/phoneAudio.stop()
+		if SaveSystem.night == 1:
+			$phone/AnimationPlayer.play("p1")
+		elif SaveSystem.night == 2:
+			$phone/AnimationPlayer.play("p2")
+		elif SaveSystem.night == 3:
+			$phone/AnimationPlayer.play("p3")
+		elif SaveSystem.night == 4:
+			$phone/AnimationPlayer.play("p4")
+			
+func _on_Timer_timeout():
+	if SaveSystem.night == 1 or SaveSystem.night == 2 or SaveSystem.night == 3 or SaveSystem.night == 4:
+		$phone/phoneAudio.play()
+		phone_waiting = true
