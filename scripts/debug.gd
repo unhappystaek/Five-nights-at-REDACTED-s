8e52ae4cb24cb6283929f68321f7b352eaed0273
unhappystaek <oli.ryn@wp.pl>
2023-05-15 21:59:49 +0200

    started debug console

---
+++ scripts/debug.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		$".".visible = false
+
+
+func _on_gold_pressed():
+	pass # Replace with function body.
+
+
+func _on_blackout_pressed():
+	pass # Replace with function body.
+
+
+func _on_hour__pressed():
+	pass # Replace with function body.
+
+
+func _on_day__pressed():
+	pass # Replace with function body.
+
+
+func _on_Jumpscares_console_pressed():
+	pass # Replace with function body.
+
+
+func _on_kill_pressed():
+	pass # Replace with function body.
